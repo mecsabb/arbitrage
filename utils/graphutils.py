36c3e1b5e2d6bb07f486f9fb0f8805b03eb99af2
Mitchell <sabbadinimitchell@gmail.com>
2023-10-29 20:55:01 -0400

    started graphutils and rlutils

---
+++ utils/graphutils.py
@@ -0,0 +1,83 @@
+'''
+graphutils - a utility module containing our main graph class definiton some useful functions
+'''
+
+import networkx as nx
+import time
+
+class Graph:
+    def __init__(self):
+        self.graph = nx.DiGraph()
+    
+    def add_node(self, name):
+        self.graph.add_node(name)
+    
+    def add_edge(self, u, v, weight=1):
+        # u, v are nodes
+        self.graph.add_edge(u, v, weight=weight)
+
+    def bellman_ford(self, source):
+        """
+        Executes the Bellman-Ford algorithm on the graph from a given source node,
+        and measures the time it takes to execute.
+        
+        Returns a tuple with the execution time followed by two dictionaries 
+        representing the distance to each node and the predecessor of each node.
+        """
+        # Check if the source node is in the graph
+        if source not in self.graph.nodes:
+            raise ValueError("Source node not found in graph")
+
+        start_time = time.time()
+
+        # Prepare the distance and predecessor for each node
+        distance = {node: float('inf') for node in self.graph.nodes}
+        predecessor = {node: None for node in self.graph.nodes}
+        distance[source] = 0
+
+        # Relax the edges
+        for _ in range(len(self.graph) - 1):
+            for u, v, attr in self.graph.edges(data=True):
+                weight = attr['weight']
+                if distance[u] + weight < distance[v]:
+                    distance[v] = distance[u] + weight
+                    predecessor[v] = u
+
+        # Check for negative weight cycles
+        for u, v, attr in self.graph.edges(data=True):
+            weight = attr['weight']
+            if distance[u] + weight < distance[v]:
+                end_time = time.time()
+                print(
+                    f"Execution time before detecting negative cycle: {end_time - start_time:.5f} seconds")
+                raise nx.NetworkXUnbounded(
+                    "Graph contains a negative weight cycle.")
+
+        end_time = time.time()
+        execution_time = end_time - start_time
+
+        return execution_time, distance, predecessor
+
+    # What else is convenient / needed?
+
+if __name__ == '__main__':
+    # Create a custom graph and add nodes and edges
+    try:
+        g = Graph()
+        g.add_node("A")
+        g.add_node("B")
+        g.add_node("C")
+
+        g.add_edge("A", "B", -1)
+        g.add_edge("B", "C", 4)
+        g.add_edge("A", "C", 3)
+
+        # Run the Bellman-Ford algorithm
+        execution_time, distance, predecessor = g.bellman_ford("A")
+
+        print(f"Bellman-Ford algorithm executed in {execution_time:.5f} seconds")
+        print("Distance:", distance)
+        print("Predecessor:", predecessor)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
