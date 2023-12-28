b9d7b922b781ff4e006ef1fff29b7385990776ed
ELLIOT THOBURN <ELLIOT.WELDON.THOBURN@gmail.com>
2023-12-27 23:10:28 -0500

    backend api for shortest path and model score. Also created sample jsons for a graph and path

--- API/backend.py
+++ API/backend.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 def shortest_path(graph):
     return graph
 
-@app.route('/process-data', methods=['POST'])
+def get_model_score(graph):
+    return graph
+
+@app.route('/process-graph', methods=['POST'])
 def process_data():
     #get json graph from request
     graph = request.json
@@ -15,6 +18,11 @@ def process_data():
 
     #return path as json
     return jsonify(path)
+
+@app.route('/evaluate-model')
+def score_model():
+    return get_model_score()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
