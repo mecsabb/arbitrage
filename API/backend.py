c0e5772fe39807767494a8338c0fcd9345f54b96
ELLIOT THOBURN <ELLIOT.WELDON.THOBURN@gmail.com>
2024-01-02 13:39:21 -0500

    small changes

--- API/backend.py
+++ API/backend.py
@@ -3,10 +3,10 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 def shortest_path(graph):
-    return graph
+    "returns a list of nodes and edges of the shortest path"
 
-def get_model_score(graph):
-    return graph
+def get_model_score():
+    "returns the score of the model"
 
 @app.route('/process-graph', methods=['POST'])
 def process_data():
@@ -18,8 +18,8 @@ def process_data():
 
 @app.route('/evaluate-model')
 def score_model():
+    #return path as json
     return jsonify(get_model_score())
-
 
 if __name__ == '__main__':
     app.run(debug=True)
