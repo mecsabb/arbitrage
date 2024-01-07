dc27e9e41c39a66fb133c2e9a28895dc7946ca75
Colin Gould <colin.gould15@gmail.com>
2024-01-07 13:20:36 -0500

    set up react router

---
+++ arbitrage-frontend/src/DisplayGraph.jsx
@@ -0,0 +1,12 @@
+function DisplayGraph() {
+    return(
+        <>
+            <h1>This is the page that displays the graph</h1>
+            <button>
+                <a href="/">Return to Homepage</a>
+            </button>
+        </>
+    )
+}
+
+export default DisplayGraph
--- arbitrage-frontend/src/main.jsx
+++ arbitrage-frontend/src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import DisplayGraph from './DisplayGraph.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "display-graph",
+    element: <DisplayGraph />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
