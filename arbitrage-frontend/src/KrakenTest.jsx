3bc770664123f747ffdc6402948d84ca351c9a2c
Isaiah Iruoha <Siruoha@gmail.com>
2024-01-23 12:10:58 -0500

    added back button

--- arbitrage-frontend/src/KrakenTest.jsx
+++ arbitrage-frontend/src/KrakenTest.jsx
@@ -24,19 +24,25 @@ const KrakenTest = () => {
   }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return (
-    <div>
-      <h2>Dogecoin (XDG) Information:</h2>
-      {tickerData !== null ? (
-        <ul>
-          <li>{`Last Price: $${tickerData.c[0]}`}</li>
-          <li>{`24h High: $${tickerData.h[1]}`}</li>
-          <li>{`24h Low: $${tickerData.l[1]}`}</li>
-          <li>{`24h Volume: ${tickerData.v[1]}`}</li>
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <>
+      <div>
+        <h2>Dogecoin (XDG) Information:</h2>
+        {tickerData !== null ? (
+          <ul>
+            <li>{`Last Price: $${tickerData.c[0]}`}</li>
+            <li>{`24h High: $${tickerData.h[1]}`}</li>
+            <li>{`24h Low: $${tickerData.l[1]}`}</li>
+            <li>{`24h Volume: ${tickerData.v[1]}`}</li>
+          </ul>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+      
+      <button>
+        <a href="/">Return to Homepage</a>
+      </button>
+    </>
   );
 };
 
