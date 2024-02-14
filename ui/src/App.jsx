f48d1dc1c7c8d8c26923b736e0e685483528dbfa
Isaiah Iruoha <Siruoha@gmail.com>
2024-02-14 15:57:17 -0500

    finalized base page

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
 
   return (
     <div className="container">
+
       <nav>
         <ul className="nav-links">
           <li className="upward"><a href="#">Home</a></li>
@@ -19,7 +20,9 @@ function App() {
           <li className="upward"><a href="#methods">Methods</a></li>
         </ul>
       </nav>
+
       <div className="content-box">
+
         <div className="content">
           <div className="text">
             <h2>A Reinforcement Learning Approach to Finding Cryptocurrency Arbitrage Paths</h2>
@@ -35,6 +38,7 @@ function App() {
           </div>
         </div>
       </div>
+
       <div className="about" id="implementation">
         <h2>Implementation</h2>
         <hr />
@@ -44,6 +48,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget justo
 
 Nulla facilisi. Ut blandit scelerisque pellentesque. Nullam id egestas lectus. Curabitur tristique magna sapien, eget dictum risus dictum varius. Nulla faucibus urna ut nibh fermentum mattis. Phasellus molestie imperdiet nunc, id pellentesque nisl cursus sit amet. Nulla vitae hendrerit nibh. Proin faucibus lacus non porttitor lacinia. Sed sagittis laoreet felis, a posuere risus sollicitudin quis. Sed auctor vitae quam ac feugiat. Pellentesque sapien lacus, luctus id dolor at, malesuada molestie sapien. Mauris quis malesuada massa, ut sollicitudin erat.</p>
       </div>
+
       <div className="about" id="arbitrage">
         <h2>Arbitrage</h2>
         <hr />
@@ -53,6 +58,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget justo
 
 Nulla facilisi. Ut blandit scelerisque pellentesque. Nullam id egestas lectus. Curabitur tristique magna sapien, eget dictum risus dictum varius. Nulla faucibus urna ut nibh fermentum mattis. Phasellus molestie imperdiet nunc, id pellentesque nisl cursus sit amet. Nulla vitae hendrerit nibh. Proin faucibus lacus non porttitor lacinia. Sed sagittis laoreet felis, a posuere risus sollicitudin quis. Sed auctor vitae quam ac feugiat. Pellentesque sapien lacus, luctus id dolor at, malesuada molestie sapien. Mauris quis malesuada massa, ut sollicitudin erat.</p>
       </div>
+
       <div className="about" id="methods">
         <h2>Methods</h2>
         <hr />
@@ -62,6 +68,15 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget justo
 
 Nulla facilisi. Ut blandit scelerisque pellentesque. Nullam id egestas lectus. Curabitur tristique magna sapien, eget dictum risus dictum varius. Nulla faucibus urna ut nibh fermentum mattis. Phasellus molestie imperdiet nunc, id pellentesque nisl cursus sit amet. Nulla vitae hendrerit nibh. Proin faucibus lacus non porttitor lacinia. Sed sagittis laoreet felis, a posuere risus sollicitudin quis. Sed auctor vitae quam ac feugiat. Pellentesque sapien lacus, luctus id dolor at, malesuada molestie sapien. Mauris quis malesuada massa, ut sollicitudin erat.</p>
       </div>
+
+      <div>
+        <a
+          href="https://www.overleaf.com/project/65c2dc3d38eab49cd8ba2af9"
+          className="btn btn-more"
+          target="_blank"
+          >Learn More <i class="fas fa-chevron-right"></i></a>
+      </div>
+
       {isPopupVisible && (
         <div className="popup visible">
           <h3>Authors</h3>
@@ -75,11 +90,28 @@ Nulla facilisi. Ut blandit scelerisque pellentesque. Nullam id egestas lectus. C
           <p>John Liu</p>
         </div>
       )}
+
       <div className="icon-background" onClick={togglePopup}>
-       <img src="/assets/author.svg" alt="authors" className={`authors ${isPopupVisible ? 'rotate' : ''}`} />
+        <svg className={`authors ${isPopupVisible ? 'rotate' : ''}`} fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+        viewBox="0 0 964.07 964.07" xml:space="preserve">
+      <g>
+        <path d="M850.662,877.56c-0.77,0.137-4.372,0.782-10.226,1.831c-230.868,41.379-273.337,48.484-278.103,49.037
+          c-11.37,1.319-19.864,0.651-25.976-2.042c-3.818-1.682-5.886-3.724-6.438-4.623c0.268-1.597,2.299-5.405,3.539-7.73
+          c1.207-2.263,2.574-4.826,3.772-7.558c7.945-18.13,2.386-36.521-14.51-47.999c-12.599-8.557-29.304-12.03-49.666-10.325
+          c-12.155,1.019-225.218,36.738-342.253,56.437l-57.445,45.175c133.968-22.612,389.193-65.433,402.622-66.735
+          c11.996-1.007,21.355,0.517,27.074,4.4c3.321,2.257,2.994,3.003,2.12,4.997c-0.656,1.497-1.599,3.264-2.596,5.135
+          c-3.835,7.189-9.087,17.034-7.348,29.229c1.907,13.374,11.753,24.901,27.014,31.626c8.58,3.78,18.427,5.654,29.846,5.654
+          c4.508,0,9.261-0.292,14.276-0.874c9.183-1.065,103.471-17.67,280.244-49.354c5.821-1.043,9.403-1.686,10.169-1.821
+          c9.516-1.688,15.861-10.772,14.172-20.289S860.183,875.87,850.662,877.56z"/>
+        <path d="M231.14,707.501L82.479,863.005c-16.373,17.127-27.906,38.294-33.419,61.338l211.087-166.001
+          c66.081,29.303,118.866,38.637,159.32,38.637c71.073,0,104.065-28.826,104.065-28.826c-66.164-34.43-75.592-98.686-75.592-98.686
+          c50.675,21.424,156.235,46.678,156.235,46.678c140.186-93.563,213.45-296.138,213.45-296.138
+          c-14.515,3.99-28.395,5.652-41.475,5.652c-65.795,0-111-42.13-111-42.13l183.144-39.885C909.186,218.71,915.01,0,915.01,0
+          L358.176,495.258C295.116,551.344,250.776,625.424,231.14,707.501z"/>
+        </g>
+        </svg>
       </div>
-      {/* 
-        add a learn more button to take you to page */}
+      
     </div>
   );
 }
